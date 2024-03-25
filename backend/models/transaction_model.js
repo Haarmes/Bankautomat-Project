21a199de75f1ff904410237862ca8b05daa01744
t2leka02 <t2leka02@students.oamk.fi>
2024-03-25 12:14:30 +0200

    basic crud methods for transaction table working

--- backend/models/transaction_model.js
+++ backend/models/transaction_model.js
@@ -12,6 +12,9 @@
             return db.query ("UPDATE transaction SET idaccount=?, amount=?, date=?, transaction_type=? WHERE transactionid=?", 
             [updateTransaction.idaccount, updateTransaction.amount, updateTransaction.date, updateTransaction.transaction_type, id], callback);
         },
+        getOneTransaction(id, callback){
+            return db.query("SELECT * FROM transaction WHERE transactionid=?", [id], callback);
+        },
         deleteTransaction(id, callback){
             return db.query("DELETE FROM transaction WHERE transactionid=?", [id], callback);
         }
--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -14,6 +14,18 @@ router.get('/', function(request,response){
     });
 });
 
+router.get('/:id', function(request,response){
+    transaction.getOneTransaction(request.params.id, function(err,result){
+        if(err){
+            response.send(err);
+        }
+        else{
+            console.log(result);
+            response.json(result[0]);
+        }
+    });
+});
+
 router.post('/', function(request,response){
     transaction.addTransaction(request.body, function(err, result){
         if(err){
@@ -26,7 +38,7 @@ router.post('/', function(request,response){
 });
 
 router.put('/:id', function(request, response){
-    transaction.updateTransaction(request.params.usern, request.body, function(err,result){
+    transaction.updateTransaction(request.params.id, request.body, function(err,result){
         if(err){
             response.send(err);
         }
