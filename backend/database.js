86c075838c51c593e1289f28dec6a14dd7e44fbb
t2leka02 <t2leka02@students.oamk.fi>
2024-04-02 11:13:41 +0300

    transaction to main final

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mysql = require('mysql2');
 const dotenv = require('dotenv');   // Käytetään dotenv että voidaan lukea ".env" tiedosto ja hakea sieltä enviromental muuttujat jotka ei mene githubiin
 // const { dot } = require('node:test/reporters');
@@ -7,12 +6,5 @@ dotenv.config();    // Luetaan .env tiedosto
 
 // Tehdään mysql yhteys
 const connection = mysql.createPool(process.env.MySQL_DB);
-=======
-const mysql=require('mysql2');
-
-const conn="mysql://bankuser:bankpass@127.0.0.1:3306/bankdatabase";
-
-const connection=mysql.createPool(conn);
->>>>>>> transaction
 
 module.exports = connection;
